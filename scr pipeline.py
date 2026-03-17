cfcefe54f75b380c95fb2be09be724d62603b62e
michelleacess-ship-it <michelleacess@gmail.com>
2026-03-17 13:43:20 -0300

    Create pipeline.py to load temperature readings

---
+++ scr pipeline.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://postgres:1234@localhost:5432/iot")
+
+df = pd.read_csv("../data/temperature_readings.csv")
+
+df.to_sql(
+    "temperature_readings",
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+print("Pipeline executado com sucesso.")
