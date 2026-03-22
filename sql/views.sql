1f326012011d5cb2ff6da29d55d7626d99d868e3
michelleacess-ship-it <michelleacess@gmail.com>
2026-03-22 16:32:53 -0300

    Criar sql/views.sql

---
+++ sql/views.sql
@@ -0,0 +1,10 @@
+CREATE VIEW avg_temp_por_dispositivo AS
+SELECT device_id, AVG(temperature) AS avg_temp
+FROM temperature_readings
+GROUP BY device_id;
+
+CREATE VIEW leituras_por_hora AS
+SELECT EXTRACT(HOUR FROM timestamp) AS hora,
+COUNT(*) AS contagem
+FROM temperature_readings
+GROUP BY hora;
